05a7340e9e834beb42f58fabff42693bca931b46
hold-door <gaotianyuxz@163.com>
2020-09-24 18:16:20 +0800

    小甲鱼入门0-4

---
+++ 0924/9_24.py
@@ -0,0 +1,53 @@
+
+'''
+print("I love python")
+print"I love python"
+'''
+
+'''
+print('------------------练习-----------------')
+temp = input("猜猜阿多喜欢的数字：")
+#guess = int(temp)
+if temp == 4:
+    print("小伙可以呀！")
+    print("哼，猜中也没有奖励！")
+else:
+    print("猜错啦，阿多心里想着的是4。")
+print("Game Over!!!")
+'''
+'''
+teacher = '阿多'
+print('teacher')
+print(teacher)
+'''
+
+
+'''
+first = 1
+second = 2
+third = first+second
+print(third)
+'''
+'''
+myteacher = '阿多'
+yourteacher = '白夜行'
+ourteacher = myteacher + yourteacher
+print(ourteacher)
+print('1')
+print('Let\'s go')
+print("Let's go")
+str = 'C:\\now'
+str
+print(str)
+str2 = r'C:\now'
+print(str2)
+'''
+str = """
+前无古人后无来者
+念天地之悠悠
+独怆然而涕下
+"""
+print(str)
+print("""前无古人后无来者
+念天地之悠悠
+独怆然而涕下""")
